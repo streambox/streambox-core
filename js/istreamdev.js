6c41c88f7913ae2c840e1f1bbfa8278a00e7f46a
mangui <g.du.pontavice@gmail.com>
2013-04-29 19:25:25 +0800

    switch to HLSProvider5.swf https://github.com/mangui/HLSprovider/blob/4839940fe62d87d18db18db284707c31288e12ea/HLSProvider5.swf?raw=true

--- js/istreamdev.js
+++ js/istreamdev.js
@@ -630,14 +630,14 @@ function playvideo(session,name) {
 					autostart: 'true',
 					plugins: { 'swf/qualitymonitor.swf' : {} },
 					modes: [
-						{ type:'flash', src:'swf/player.swf', config: { provider:'swf/adaptiveProvider.swf', file:'ram/sessions/session' +session +'/stream.m3u8' } },
+						{ type:'flash', src:'swf/player.swf', config: { provider:'swf/HLSProvider5.swf', file:'ram/sessions/session' +session +'/stream.m3u8' } },
 						{ type:'html5', config: { file:'ram/sessions/session' +session +'/stream.m3u8' } }
 					]});
 			else
 				jwplayer("mediaplayer").setup({
 					width: playerWidth,height: playerHeight,
 					modes: [
-						{ type:'flash', src:'swf/player.swf', config: { provider:'swf/adaptiveProvider.swf', file:'ram/sessions/session' +session +'/stream.m3u8' } },
+						{ type:'flash', src:'swf/player.swf', config: { provider:'swf/HLSProvider5.swf', file:'ram/sessions/session' +session +'/stream.m3u8' } },
 						{ type:'html5', config: { file:'ram/sessions/session' +session +'/stream.m3u8' } }
 					]});
 		   }
