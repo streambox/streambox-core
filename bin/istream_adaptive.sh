56ae2a798f570d8f977ae3a059bc18731c60376a
mangui <g.du.pontavice@gmail.com>
2013-05-08 10:55:24 +0800

    fill SDT with service provider

--- bin/istream_adaptive.sh
+++ bin/istream_adaptive.sh
@@ -98,7 +98,7 @@ do
 	echo "`get_playlist_name $streamid`" >> stream.m3u8
 done
 
-COMMON_OPTION="-map 0:v:0 -map 0:a:0 -filter:v yadif -f mpegts -async 2 -threads 0 "
+COMMON_OPTION="-map 0:v:0 -map 0:a:0 -filter:v yadif -f mpegts -async 2 -threads 0 -metadata service_provider=streambox "
 AUDIO_OPTION="-acodec libaacplus -ac 2 -b:a "
 VIDEO_OPTION="-vcodec libx264 -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 -me_method hex -subq 7 -trellis 1 -refs 5 -coder 0 -me_range 16 -i_qfactor 0.71 -rc_eq 'blurCplx^(1-qComp)' -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -level 30 -sc_threshold 0 "
 
